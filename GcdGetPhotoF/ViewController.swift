e0e9dd7e29b319efe59e9d9fcd8e372dd2613047
Fausto Checa <fausto.checa@gmail.com>
2017-05-08 01:09:03 +0200

    IOS

--- GcdGetPhotoF/ViewController.swift
+++ GcdGetPhotoF/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +26,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func getPhoto(_ sender: Any) {
+        
+        let url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
+        
+        if let actualUrl = url {
+            
+            // Go to background thread
+            DispatchQueue.global(qos: .userInitiated).async {
+                
+                let imageData = NSData(contentsOf: actualUrl)! as Data
+                
+                // Go to main thread to update UI
+                DispatchQueue.main.async {
+                    
+                    let image = UIImage(data: imageData)
+                    self.imageView.image = image
+                    
+                }
+                
+            }
+         
+        }
+        
+    }
 
 }
 
